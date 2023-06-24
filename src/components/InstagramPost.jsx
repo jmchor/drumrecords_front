dd76ffdfcbf71f62e2cc6fa6b10969ee21e35520
Johannes Chorzempa <jchorzempa@posteo.org>
2023-06-24 22:08:16 +0200

    Update component to add size changes

--- src/components/InstagramPost.jsx
+++ src/components/InstagramPost.jsx
@@ -1,13 +1,26 @@
 import { InstagramEmbed } from 'react-social-media-embed';
+import { useState } from 'react';
+import Button from '@mui/material/Button';
 
-const InstagramPost = ({instaURL}) => {
+const InstagramPost = ({ instaURL }) => {
+	const [isVideoExpanded, setIsVideoExpanded] = useState(false);
 
+	const handleToggleSize = () => {
+		setIsVideoExpanded(!isVideoExpanded);
+	};
 
-  return (
-    <div>
-  <InstagramEmbed url={instaURL} height={460} />
-</div>
-  )
-}
+	const videoClassName = isVideoExpanded ? 'expanded' : '';
 
-export default InstagramPost
+	return (
+		<div onClick={handleToggleSize} className='p-5'>
+			<Button className='enlarge-button ' onClick={handleToggleSize}>
+				{isVideoExpanded ? 'Shrink Video' : 'Enlarge Video'}
+			</Button>
+			<div className='video-container flex justify-center content-center items-center mt-2'>
+      <InstagramEmbed url={instaURL} className={isVideoExpanded ? 'expanded' : 'shrink'} />
+			</div>
+		</div>
+	);
+};
+
+export default InstagramPost;
