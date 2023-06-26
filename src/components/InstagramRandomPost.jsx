bb4535e1d2b530a4ec0856feb574a3e9b4651160
Johannes Chorzempa <jchorzempa@posteo.org>
2023-06-27 00:09:31 +0200

    Remove 0 padding

--- src/components/InstagramRandomPost.jsx
+++ src/components/InstagramRandomPost.jsx
@@ -3,7 +3,7 @@ import { InstagramEmbed } from 'react-social-media-embed';
 const InstagramRandomPost = ({ instaURL }) => {
 	return (
 		<div className='p-0'>
-			<div className='p-0 video-container flex justify-center content-center items-center mt-2'>
+			<div className=' video-container flex justify-center content-center items-center mt-2'>
 				<InstagramEmbed url={instaURL} className='expanded'/>
 			</div>
 		</div>
