3b574e38ed81eaa193f652edb7999f97c358e94e
Johannes Chorzempa <jchorzempa@posteo.org>
2023-06-26 14:05:08 +0200

    Create new landing page

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,32 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import InstagramRandomPost from '../components/InstagramRandomPost.jsx';
+
 
 const Homepage = () => {
-  return (
-    <div>Homepage</div>
-  )
-}
+	const API = import.meta.env.VITE_API;
 
-export default Homepage
+	const [randomRecord, setRandomRecord] = useState(null);
+
+	useEffect(() => {
+		axios
+			.get(`${API}/random`)
+			.then((response) => {
+				console.log(response.data[0]);
+				setRandomRecord(response.data[0]);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
+
+	if (randomRecord) {
+		return (
+			<div>
+				<InstagramRandomPost instaURL={randomRecord.url} />
+			</div>
+		);
+	}
+};
+
+export default Homepage;
