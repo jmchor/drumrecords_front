37102f6fc4f72f4fbcf03ecd2119a48fd1ce7963
Johannes Chorzempa <jchorzempa@posteo.org>
2023-06-27 12:18:53 +0200

    Create Admin Panel

---
+++ src/pages/AdminPanel.jsx
@@ -0,0 +1,22 @@
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { CardActionArea } from '@mui/material';
+import { Link } from 'react-router-dom';
+import Boxcard from '../components/Boxcard';
+
+
+const AdminPanel = () => {
+	return (
+		<div className='flex justify-center items-center h-100'>
+			<Box className='flex justify-center items-center flex-wrap'>
+				<Boxcard path={'/new-record'} label={"/images/create.png"} />
+				<Boxcard path={'/update-record'} label={"/images/update.png"} />
+				<Boxcard path={'/delete-record'} label={"/images/destroy.png"} />
+				<Boxcard path={'/update-profile'} label={"/images/profileNew.png"} />
+			</Box>
+		</div>
+	);
+};
+
+export default AdminPanel;
