ef0c2ac553627d2fe95e32491441eaef01657095
Johannes Chorzempa <jchorzempa@posteo.org>
2023-06-27 15:12:58 +0200

    Add AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter as Router} from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthProvider } from './context/auth.context.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<Router>
+			<AuthProvider>
+				<App />
+			</AuthProvider>
+		</Router>
+	</React.StrictMode>
+);
