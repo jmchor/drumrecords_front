1486d43343fbf5e26f7e91ac26e1e400a7a2f992
Johannes Chorzempa <jchorzempa@posteo.org>
2023-06-27 15:14:05 +0200

    Create create Record form

--- src/pages/CreateRecord.jsx
+++ src/pages/CreateRecord.jsx
@@ -1,8 +1,81 @@
+import Button from '@mui/material/Button';
+import Selector from '../components/Selector';
+import { useState } from 'react';
+import axios from 'axios';
 
 const CreateRecord = () => {
-  return (
-    <div>CreateRecord</div>
-  )
-}
+	const API = import.meta.env.VITE_API;
 
-export default CreateRecord
+	const [url, setURL] = useState('');
+	const [artist, setArtist] = useState('');
+	const [selectedCategory, setSelectedCategory] = useState('');
+	const [errorMessage, setErrorMessage] = useState('');
+
+	const handleCategoryChange = (category) => {
+		setSelectedCategory(category);
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const newRecord = {
+			url: url,
+			artist: artist,
+			category: selectedCategory,
+		};
+
+		console.log(newRecord);
+		axios.post(`${API}/records`, newRecord).then((response) => {
+			console.log(response.data);
+			setURL('');
+			setArtist('');
+			setSelectedCategory('');
+		});
+	};
+
+	const fixedInputClass =
+		'rounded-md appearance-none relative block w-full px-3 py-2 my-4 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-purple-500 focus:border-purple-500 focus:z-10 sm:text-sm';
+
+	return (
+		<div className='my-3'>
+			<form className='flex flex-col mx-auto' onSubmit={handleSubmit}>
+				<h4 className='text-2xl text-slate-600 my-3'>Create Item</h4>
+				<label htmlFor='name' className='text-xl'>
+					URL
+				</label>
+				<input
+					type='text'
+					id='name'
+					className={fixedInputClass}
+					value={url}
+					onChange={(event) => setURL(event.target.value)}
+				/>
+				<label htmlFor='author' className='text-xl'>
+					Artist
+				</label>
+				<input
+					type='text'
+					id='description'
+					className={fixedInputClass}
+					value={artist}
+					onChange={(event) => setArtist(event.target.value)}
+				/>
+
+				<label htmlFor='categoryArray' className='text-xl'>
+					Category
+				</label>
+
+				<Selector onCategoryChange={handleCategoryChange} recordCollection={null} setRecordCollection={null} />
+
+				<div>
+					<Button variant='contained' type='submit' className='text-xl mt-3'>
+						Add
+					</Button>
+				</div>
+			</form>
+			{/* Error message */}
+			<div className='my-2'>{errorMessage && <p className='text-danger'>{errorMessage}</p>}</div>
+		</div>
+	);
+};
+
+export default CreateRecord;
