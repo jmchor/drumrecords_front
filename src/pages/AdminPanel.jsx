0e838e3315566886cc15587e8c49253ecf6f93bb
Johannes Chorzempa <jchorzempa@posteo.org>
2023-06-28 11:29:25 +0200

    Flexbox it!

--- src/pages/AdminPanel.jsx
+++ src/pages/AdminPanel.jsx
@@ -8,13 +8,15 @@ import Boxcard from '../components/Boxcard';
 
 const AdminPanel = () => {
 	return (
-		<div className='flex justify-center items-center h-100'>
-			<Box className='flex justify-center items-center flex-wrap'>
-				<Boxcard path={'/new-record'} label={"/images/create.png"} />
-				<Boxcard path={'/update-record'} label={"/images/update.png"} />
-				<Boxcard path={'/delete-record'} label={"/images/destroy.png"} />
-				<Boxcard path={'/update-profile'} label={"/images/profileNew.png"} />
-			</Box>
+		<div className='flex justify-center panel'>
+			<div className='flex justify-center items-center'>
+				<Box className='flex justify-center items-center flex-wrap'>
+					<Boxcard path={'/new-record'} label={"/images/create.png"} />
+					<Boxcard path={'/update-record'} label={"/images/update.png"} />
+					<Boxcard path={'/delete-record'} label={"/images/destroy.png"} />
+					<Boxcard path={'/update-profile'} label={"/images/profileNew.png"} />
+				</Box>
+			</div>
 		</div>
 	);
 };
