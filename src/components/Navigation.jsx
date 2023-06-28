8008babe6a4cda090d724d7eb68dc8688da847b6
Johannes Chorzempa <jchorzempa@posteo.org>
2023-06-28 11:29:07 +0200

    More flexbox, replace MobileNav with Collapse

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Navbar, MobileNav, Typography, IconButton } from '@material-tailwind/react';
+import { Navbar, MobileNav, Typography, IconButton, Collapse } from '@material-tailwind/react';
 import { AuthContext } from '../context/auth.context';
 import { useContext } from 'react';
 import LogoutIcon from "@mui/icons-material/Logout";
@@ -17,26 +17,26 @@ const Navigation = () => {
 	const navList = (
 		<ul className='mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6'>
 			<Typography as='li' variant='small' color='blue-gray' className='p-1 font-normal'>
-				<a href='/' className='flex items-center'>
+				<a href='/' className='flex items-center justify-center'>
 					Random Drum Record
 				</a>
 			</Typography>
 			<Typography as='li' variant='small' color='blue-gray' className='p-1 font-normal'>
-				<a href='/all-records' className='flex items-center'>
+				<a href='/all-records' className='flex items-center justify-center'>
 					All Records
 				</a>
 			</Typography>
 			<Typography as='li' variant='small' color='blue-gray' className='p-1 font-normal'>
-				<a href='#' className='flex items-center'>
+				<a href='#' className='flex items-center justify-center'>
 					About
 				</a>
 			</Typography>
 			{isLoggedIn ? (<Typography as='li' variant='small' color='blue-gray' className='p-1 font-normal'>
-				<a href='/admin' className='flex items-center'>
+				<a href='/admin' className='flex items-center justify-center'>
 					Admin
 				</a>
 			</Typography>) : (<Typography as='li' variant='small' color='blue-gray' className='p-1 font-normal'>
-				<a href='/login' className='flex items-center'>
+				<a href='/login' className='flex items-center justify-center'>
 					Admin
 				</a>
 			</Typography>) }
@@ -53,13 +53,13 @@ const Navigation = () => {
 	);
 
 	return (
-		<Navbar id='nav-bar' className='mx-auto max-w-screen-xl py-2 lg:py-4'>
-			<div className='container mx-auto flex items-center justify-center text-blue-gray-900'>
+		<Navbar id='nav-bar' className='flex flex-col bg-slate-800 mx-auto py-2 lg:py-4'>
+			<div className=' mx-auto flex items-center justify-center text-blue-gray-900'>
 				<div className='hidden lg:block'>{navList}</div>
 
 				<IconButton
 					variant='text'
-					className='ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden'
+					className='ml-auto text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden'
 					ripple={false}
 					onClick={() => setOpenNav(!openNav)}
 				>
@@ -69,7 +69,7 @@ const Navigation = () => {
 							fill='none'
 							className='h-6 w-6'
 							viewBox='0 0 24 24'
-							stroke='currentColor'
+							stroke='white'
 							strokeWidth={2}
 						>
 							<path strokeLinecap='round' strokeLinejoin='round' d='M6 18L18 6M6 6l12 12' />
@@ -79,7 +79,7 @@ const Navigation = () => {
 							xmlns='http://www.w3.org/2000/svg'
 							className='h-6 w-6'
 							fill='none'
-							stroke='currentColor'
+							stroke='white'
 							strokeWidth={2}
 						>
 							<path strokeLinecap='round' strokeLinejoin='round' d='M4 6h16M4 12h16M4 18h16' />
@@ -87,9 +87,9 @@ const Navigation = () => {
 					)}
 				</IconButton>
 			</div>
-			<MobileNav open={openNav}>
-				<div className='container mx-auto'>{navList}</div>
-			</MobileNav>
+			<Collapse open={openNav}>
+				<div className='container mx-auto flex justify-center text-center'>{navList}</div>
+			</Collapse>
 		</Navbar>
 	);
 };
