a96e0e01b37ab082e2173003a93bad622dae9cd8
Johannes Chorzempa <jchorzempa@posteo.org>
2023-06-29 14:59:13 +0200

    Alter loading spinner

--- src/components/IsPrivate.jsx
+++ src/components/IsPrivate.jsx
@@ -3,7 +3,7 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
-import { MoonLoader } from "react-spinners";
+import { PacmanLoader } from "react-spinners";
 
 function IsPrivate({ children }) {
   const { isLoggedIn, isLoading } = useContext(AuthContext);
@@ -12,7 +12,7 @@ function IsPrivate({ children }) {
     return (
       <section id="main-content">
         <div id="main-section" className="p-4 my-4 flex justify-center w-100">
-          <MoonLoader color="#1976D2" size={30} />
+          <PacmanLoader color="#1976D2" size={30} />
         </div>
       </section>
     );
